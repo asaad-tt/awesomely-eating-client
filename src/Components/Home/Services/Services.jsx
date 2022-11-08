b9f6f0fc16ac6425b38431227a2c94e4157389a4
asaad-tt <aasadshumon86@gmail.comm>
2022-11-08 10:40:06 +0600

    banner added

---
+++ src/Components/Home/Services/Services.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <div>
+      <div>
+        <p className="text-2xl font-bold text-violet-600">Services</p>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
