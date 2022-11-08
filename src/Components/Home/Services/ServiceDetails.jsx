1b7eaf6443bd303ff8d81de56757f265a28afbaa
asaad-tt <aasadshumon86@gmail.comm>
2022-11-09 00:50:15 +0600

    react photo view added

--- src/Components/Home/Services/ServiceDetails.jsx
+++ src/Components/Home/Services/ServiceDetails.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import "react-photo-view/dist/react-photo-view.css";
 
 const ServiceDetails = () => {
   const service = useLoaderData();
@@ -13,7 +15,11 @@ const ServiceDetails = () => {
         <div>
           <div className="card card-compact w-full bg-base-100 shadow-xl">
             <figure>
-              <img src={img} alt="biriany" />
+              <PhotoProvider>
+                <PhotoView src={img}>
+                  <img src={img} alt="biriany" />
+                </PhotoView>
+              </PhotoProvider>
             </figure>
             <div className="card-body">
               <h2 className="card-title">{title}</h2>
