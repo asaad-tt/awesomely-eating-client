7db341c624ba9d426d607947d00207445527a5e9
asaad-tt <aasadshumon86@gmail.comm>
2022-11-09 23:34:37 +0600

    add service done

--- src/Components/AddService/AddService.jsx
+++ src/Components/AddService/AddService.jsx
@@ -1,27 +1,76 @@
-import React from "react";
+import React, { useContext } from "react";
+import { toast } from "react-toastify";
+import { AuthContext } from "../../Context/UserContext";
 
 const AddService = () => {
+  const { user } = useContext(AuthContext);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = user?.email;
+    const title = form.title.value;
+    const img = form.img.value;
+    const price = form.price.value;
+    const description = form.description.value;
+
+    const addServices = {
+      email,
+      title,
+      img,
+      price,
+      description,
+    };
+
+    fetch("http://localhost:5000/services", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(addServices),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.acknowledged) {
+          toast.info("service added successfull");
+          form.reset();
+        }
+      });
+  };
+
   return (
-    <div>
+    <div className="max-w-screen-xl mx-auto w-4/6">
       <h2>Add service item </h2>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
+          name="title"
           placeholder="service name"
-          className="input input-bordered input-accent w-full max-w-xs"
-        />
+          className="input input-bordered input-accent w-full mb-3"
+        />{" "}
+        <br />
         <input
           type="text"
+          name="img"
           placeholder="photo url"
-          className="input input-bordered input-accent w-full max-w-xs"
+          className="input input-bordered input-accent w-full mb-3"
           required
-        />
+        />{" "}
+        <br />
         <input
           type="text"
+          name="price"
           placeholder="price"
-          className="input input-bordered input-accent w-full max-w-xs"
+          className="input input-bordered input-accent w-full mb-3"
           required
-        />
+        />{" "}
+        <br />
+        <textarea
+          className="textarea textarea-success w-full"
+          name="description"
+          placeholder="Bio"
+        ></textarea>
+        <input type="submit" className="btn btn-primary" value="Add Service" />
       </form>
     </div>
   );
