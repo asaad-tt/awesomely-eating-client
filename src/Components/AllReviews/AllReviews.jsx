126cb9859b0cd79c2395d2cd9d96e1c7eb38c756
asaad-tt <aasadshumon86@gmail.comm>
2022-11-09 19:11:42 +0600

    all reviews added

--- src/Components/AllReviews/AllReviews.jsx
+++ src/Components/AllReviews/AllReviews.jsx
@@ -1,13 +1,42 @@
 import React, { useEffect, useState } from "react";
+import Reviews from "./Reviews";
 
 const AllReviews = () => {
   const [reviews, setReviews] = useState([]);
+  const [refresh, setRefresh] = useState(false);
 
   useEffect(() => {
-    fetch("http://localhost:5000/reviews");
-  }, []);
+    fetch("http://localhost:5000/reviews")
+      .then((res) => res.json())
+      .then((data) => {
+        setRefresh(!refresh);
+        setReviews(data);
+      })
+      .catch((error) => console.log(error));
+  }, [refresh]);
 
-  return <div>hello reviews</div>;
+  return (
+    <div>
+      <div className="overflow-x-auto w-full">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {reviews.map((review) => (
+              <Reviews key={review._id} review={review}></Reviews>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default AllReviews;
---
+++ src/Components/AllReviews/Reviews.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import avatar from "../../asset/avatar/avatar.png";
+
+const Reviews = ({ review }) => {
+  console.log(review);
+  const { email, text, uid, userName, photoURL } = review;
+  return (
+    <div>
+      <tr>
+        <th>
+          <label></label>
+        </th>
+        <td>
+          <div className="flex items-center justify-center space-x-3">
+            <div className="avatar">
+              <div className="rounded w-24 h-24">
+                {review?.photoURL ? (
+                  <img width="60" height="60" src={photoURL} alt="" />
+                ) : (
+                  <img width="60" height="60" src={avatar} alt="" />
+                )}
+              </div>
+            </div>
+            <div>
+              <div className="font-bold">{userName}</div>
+              <div className="text-sm opacity-50">{email}</div>
+            </div>
+          </div>
+        </td>
+        <td>{text}</td>
+
+        <th></th>
+      </tr>
+    </div>
+  );
+};
+
+export default Reviews;
